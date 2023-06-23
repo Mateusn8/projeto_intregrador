bb3182308e7421a9e34de2978143654bd26552c9
Mateusn8 <128602978+Mateusn8@users.noreply.github.com>
2023-06-23 13:07:37 -0300

    Add files via upload

--- treinador.py
+++ treinador.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import random 
 
 class treinador:
     def __init__(self, nome, idade, cidade, pokedex_treinador):
@@ -6,34 +7,67 @@ class treinador:
         self.idade = idade
         self.cidade = cidade
         self.pokedex = pokedex_treinador
-       
+        self.conexao = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Mateus1234",
+            database="projeto_integrado"
+        )
+        self.cursor = self.conexao.cursor()
+    
+    def pokedex_treinador(self):
+        sql = "CREATE TABLE pokemon_coletados (id_treinador INT PRIMARY KEY, nome_treinador VARCHAR(50), id_pokemon INT, nome_pokemon VARCHAR(50))"
+        self.cursor.execute(sql)
+        self.conexao.commit()       
 class Pokedex:
     def __init__(self):
         self.conexao = mysql.connector.connect(
             host="localhost",
             user="root",
             password="Mateus1234",
-            database="projeto_integrado"ss
+            database="projeto_integrado"
         )
         self.cursor = self.conexao.cursor()
         
     def adicionar_pokemon(self, pokemon):
-        self.pokedex_treinador.append(pokemon)
+        self.pokemon = pokemon
+        sql = "INSERT INTO pokemon_coletados (id_pokemon,id_treinador)VALUES(%s,%s)"
+        valores = (self.id_pokemon,self.id_treinador)
+        self.cursor.execute(sql,valores)
+        self.conexao.commit()
+        print("Pokémon capturado com sucesso.")
         
     def lista_pokemon(self):
-        self.pokedex_treinador = []
-
-    def nomea_pokemon(self):
-        ...
-
-    def procura_pokemon(self, nome):
-        for pokemon in self.pokemon_list:
-            if pokemon.nome.lower() == nome.lower():
-                return pokemon
-        return None
+        sql = "SELECT * FROM pokemon_coletados"
+        self.cursor.execute(sql)
+        pokemons_capturados = self.cursor.fetchall()
+        for pokemon in pokemons_capturados:
+            print(pokemon)
     
-    def atualiza_pokemon(self):
-        ...
+    def solta_pokemon(self, id_pokemon):
+        sql = "DELETE FROM pokemon_coletados WHERE id_pokemon = %s"
+        valor = (id_pokemon)
+        self.cursor.execute(sql, valor)
+        self.conexao.commit()
+        print("Pokémon solto com sucesso.")
+    
+    def nomea_pokemon(self, id_pokemon, novo_nome):
+        sql = f"UPDATE {self.nome} SET nome = %s WHERE id_pokemon = %s"
+        valores = (novo_nome, id_pokemon)
+        self.cursor.execute(sql, valores)
+        self.conexao.commit()
+        
+    def procura_pokemon(self, nome):
+        sql = "SELECT * FROM pokemon_coletados WHERE nome = %s"
+        valor = (nome)
+        self.cursor.execute(sql, valor)
+        resultado = self.cursor.fetchone()
+        if resultado is None:
+            print('pokémon não encontrado')
+        
+        else:
+            print(resultado)
+            
     
     def fecha_conexao(self):
         self.cursor.close()
